a2a34e6ce940d35797eacb8733dab93181803873
Dhananjaydk15 <dhananjaykhairnar15@gmail.com>
2025-07-21 11:09:58 +0530

    Auto commit from Jenkins

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 
 class Test(APIView):
     def get(self,request,*args,**kwargs):
-        return Response("test jenkins pipeline with nginx cdac")
+        return Response("test jenkins pipeline with nginx cdac 2")
         
 
 
